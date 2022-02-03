0a6a25ec2722edffaf77035f6c333ef7a53ba58d
developython14 <djmel1955@gmail.com>
2022-02-03 08:14:28 +0600

    structring the app

--- static/index.js
+++ static/index.js
@@ -1,1 +1,27 @@
-alert('hi every one hgow are you ')
+const api_url = 'https://www.breakingbadapi.com/api/characters' ; 
+
+
+async function getdata() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const toma = data.map(m => m.name)
+    put(toma);
+}
+
+function put(data){
+    var sel = document.querySelector('#sel')
+    for (let element of data) {
+        sel.innerHTML = sel.innerHTML+  `<option selected>${element}</option>`
+    }
+}
+
+getdata();
+async function getchar(element){
+    var sel = document.querySelector('#oper')
+    const response = await fetch(api_url + '?name=' + element);
+    const data = await response.json();
+    sel.innerHTML =  `<h2> ${data[0].name} </h2>
+    <h4> ${data[0].portrayed} </h4>
+    <img src="${data[0].img}" alt="..." width= 250px>
+    `
+}
